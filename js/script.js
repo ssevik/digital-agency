b3fec160466193a2dfe3ba71fae8014b22f97b2e
Artem <talibanssevik@gmail.com>
2021-12-24 19:14:59 +0200

    desc css

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var swiper = new Swiper(".hero-slider", {
+  loop: true,
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+});
+      
+var swiper = new Swiper(".testimonial-slider", {
+   
+  loop: true,
+        pagination: {
+          el: ".swiper-pagination",
+          dynamicBullets: true,
+          clickable: true,
+        },
+      });
